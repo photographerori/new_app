24d2d22cf7e302911738213ad5a0acf29ae3d163
photographerori <takataka.malong@gmail.com>
2020-08-05 21:29:57 +0900

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0RbEOx8h7oNQKrkDbVUHp2kTDpKHSAd4",
+  authDomain: "vue-fire-ffb9d.firebaseapp.com",
+  databaseURL: "https://vue-fire-ffb9d.firebaseio.com",
+  projectId: "vue-fire-ffb9d",
+  storageBucket: "vue-fire-ffb9d.appspot.com",
+  messagingSenderId: "843917389865",
+  appId: "1:843917389865:web:c0b7686aa79619ca3cec38",
+  measurementId: "G-7EQVFXK2HE"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export default app.firestore();
