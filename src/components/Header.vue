ed60129f37cd7881702c9ef23ba4776577c31358
photographerori <takataka.malong@gmail.com>
2020-08-09 23:53:20 +0900

    add LikesCount and Header

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Hello Vue.js</p>
+    <p>Hello Vue.js</p>
+    <p>Hello Vue.js</p>
+    <p>Hello Vue.js</p>
+    <p>Hello Vue.js</p>
+    <p>Hello Vue.js</p>
+    <p>Hello Vue.js</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
