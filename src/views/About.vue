059b483515debacaca91e2645fa1106bc512d2b3
photographerori <takataka.malong@gmail.com>
2020-08-19 19:11:10 +0900

    changed about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
     <h1>This is About page.</h1>
+    <div>
+      <LikeHeader></LikeHeader>
+      <LikeNumber></LikeNumber>
+      <Tweet></Tweet>
+    </div>
   </div>
 </template>
+
+<script>
+import LikeHeader from "../components/LikeHeader";
+import LikeNumber from "../components/LikeNumber";
+import Tweet from "../components/Tweet";
+export default {
+  components: {
+    LikeHeader,
+    LikeNumber,
+    Tweet,
+  },
+};
+</script>
