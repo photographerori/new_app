ff2304d9b4deffa5ce4ea89197d67369d6595b89
photographerori <takataka.malong@gmail.com>
2020-08-19 14:30:18 +0900

    install vue vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,56 @@
 <template>
-  <div>
-    <LikeHeader></LikeHeader>
-    <h2>いいねの合計：{{ number }}</h2>
-    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-    <Tweet></Tweet>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import LikeHeader from "./components/LikeHeader.vue";
-import Tweet from "./components/Tweet.vue";
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  data: function() {
-    return {
-      number: 10,
-    };
-  },
+  name: "App",
+
   components: {
-    LikeHeader,
-    Tweet,
+    HelloWorld,
   },
-  methods: {
-    incrementNumber(value) {
-      this.number = value;
-    },
-  },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import LikeNumber from "./components//LikeNumber.vue";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 Vue.component("LikeNumber", LikeNumber);
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App),
 }).$mount("#app");
