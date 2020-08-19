bfa572eef3780d4ef1225e393af505f5e8cb9813
photographerori <takataka.malong@gmail.com>
2020-08-19 18:55:20 +0900

    add comment function

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,35 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
+import Comments from "../views/Comments.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "Home",
     component: Home,
   },
   {
-    path: "/",
+    path: "/about",
     name: "About",
     component: About,
+  },
+  {
+    path: "/comment",
+    name: "Comments",
+    component: Comments,
+  },
+  {
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    component: Register,
   },
 ];
 
