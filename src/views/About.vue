1d9b6ad4c4c5182e9e25d115d2817655e4e0f0b9
photographerori <takataka.malong@gmail.com>
2020-08-19 19:45:37 +0900

    add SignUp.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,6 +5,7 @@
       <LikeHeader></LikeHeader>
       <LikeNumber></LikeNumber>
       <Tweet></Tweet>
+      <SignUp></SignUp>
     </div>
   </div>
 </template>
@@ -13,11 +14,13 @@
 import LikeHeader from "../components/LikeHeader";
 import LikeNumber from "../components/LikeNumber";
 import Tweet from "../components/Tweet";
+import SignUp from "../components/SignUp";
 export default {
   components: {
     LikeHeader,
     LikeNumber,
     Tweet,
+    SignUp,
   },
 };
 </script>
