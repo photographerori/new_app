414b92966434fae701f3644f7e2351a7d45eb46e
photographerori <takataka.malong@gmail.com>
2020-08-21 20:03:40 +0900

    coding register and login functions

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,33 @@
 <template>
-  <div>
+  <div class="login">
     <h2>ログイン</h2>
-    <label for="email">Email:</label>
-    <input id="email" type="email" v-model="email" />
-    <br />
-    <label for="password">パスワード:</label>
-    <input id="password" type="password" v-model="password" />
-    <br />
-    <button @click="login">送信</button>
+    <div>
+      <button>Googleアカウントでログイン</button>
+    </div>
+    <div>
+      <button>Facebookアカウントでログイン</button>
+    </div>
+    <div>
+      <button>メールアドレスでログイン</button>
+      <div>
+        <label for="email">Email: </label>
+        <input id="email" type="email" v-model="email" />
+      </div>
+      <div>
+        <label for="password">パスワード: </label>
+        <input id="password" type="password" v-model="password" />
+      </div>
+      <button @click="login">ログイン</button>
+      <p>まだアカウントを作成していない方はこちらから</p>
+      <router-link to="/register">登録</router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
 export default {
+  name: "Login",
   data() {
     return {
       email: "",
@@ -21,8 +36,61 @@ export default {
   },
   methods: {
     login() {
-      console.log("Clicked!");
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch(function(error) {
+          alert(error.message);
+        });
     },
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.email = user.email;
+        this.password = user.password;
+        this.user = user;
+      } else {
+        this.user = null;
+      }
+    });
   },
 };
 </script>
+
+<style scoped>
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  color: white;
+  background-color: slategray;
+  margin: 15px auto;
+}
+.login {
+  margin-top: 20px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
+input {
+  margin: 10px 0;
+  border: 1px solid black;
+}
+</style>
